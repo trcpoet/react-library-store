83be70779b1e9778fc5e4e210043986f6354903b
Tahsin Chowdhury <tchowdhury29@gmail.com>
2025-08-27 13:19:35 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import {library } from '@fortawesome/fontawesome-svg-core'
+import {faBars, faShoppingCart, faTimes} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faShoppingCart, faTimes)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +19,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// index.js sets up all the tooling 
