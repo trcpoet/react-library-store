3bba4606196ef662b1c3ba6b6fba9522067433c9
Tahsin Chowdhury <tchowdhury29@gmail.com>
2025-09-07 02:31:34 -0500

    W

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
